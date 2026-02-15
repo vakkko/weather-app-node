92b18095b6232826e1af9dad65e06f37ce95989e
Vladimer Gabisonia <vakogabo44@gmail.com>
2026-02-15 22:38:48 +0400

    feat: add form element and fetch data based on input value

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
-fetch("http://localhost:5000/weather?address=Boston")
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchValue = search.value;
+  try {
+    fetch(`http://localhost:5000/weather?address=${searchValue}`)
+      .then((response) => {
+        if (!response.ok) throw new Error("Something went wrong");
+        return response.json();
+      })
+      .then((data) => console.log(data.message));
+  } catch (err) {
+    console.log(err);
+  }
+});
